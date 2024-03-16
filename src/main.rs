7a4c95fbf605dafd4570eea21a65126fadbae813
graftss <nfireman1@gmail.com>
2024-03-15 20:36:23 -0400

    initial commit with the goofy bevy config hacks

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+const X_EXTENT: f32 = 600.;
+const GRID_SIZE: u8 = 3;
+
+fn spawn_grid(mut commands: Commands) {
+    for x in 0..GRID_SIZE {
+        for y in 0..GRID_SIZE {}
+    }
+}
+
+/// The marker component for the parent of all grid tiles.
+#[derive(Component)]
+struct GridRoot;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    const TILE_WIDTH: f32 = 100.0;
+    const TILE_HEIGHT: f32 = 100.0;
+    const TILE_PADDING_X: f32 = 5.0;
+    const TILE_PADDING_Y: f32 = 5.0;
+
+    let grid_width = 3;
+    let grid_height = 3;
+
+    let tile_color = Color::hsl(200.0, 0.5, 0.4);
+
+    let grid_root = (
+        GridRoot,
+        SpatialBundle {
+            visibility: Visibility::Visible,
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+    );
+
+    commands.spawn(grid_root).with_children(|parent| {
+        let mut x = 0.0;
+        let mut y = 0.0;
+
+        for _ in 0..grid_width {
+            for _ in 0..grid_height {
+                let rect_mesh = Mesh2dHandle(meshes.add(Rectangle::new(TILE_WIDTH, TILE_HEIGHT)));
+                parent.spawn(MaterialMesh2dBundle {
+                    mesh: rect_mesh,
+                    material: materials.add(tile_color),
+                    transform: Transform::from_xyz(x, y, 0.0),
+                    ..default()
+                });
+
+                y += TILE_HEIGHT + TILE_PADDING_Y;
+            }
+            x += TILE_WIDTH + TILE_PADDING_X;
+        }
+    });
+}
